8a4c31753349998bb5154c8707280680cdf6ae8f
sachinambalkar <sambalka@hawk.iit.edu>
2020-01-22 20:42:09 -0500

    added aws changes

--- src/main/java/com/staxrt/tutorial/Application.java
+++ src/main/java/com/staxrt/tutorial/Application.java
@@ -1,7 +1,14 @@
 package com.staxrt.tutorial;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * The type Application.
@@ -9,8 +16,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Givantha Kalansuriya
  */
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
 
+
+    @Autowired
+    private Environment env;
   /**
    * The entry point of application.
    * @param args the input arguments
@@ -18,4 +28,23 @@ public class Application {
   public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/products").allowedOrigins("http://localhost:8080");
+            }
+        };
+    }
+
+	@Override
+	public void run(String... args )throws Exception {
+        System.out.println("*########********* spring.datasource.url: "+env.getProperty("spring.datasource.url"));
+
+        System.out.println("#############********** spring.datasource.username: "+env.getProperty("spring.datasource.username"));
+        System.out.println("#############********** spring.datasource.password: "+env.getProperty("spring.datasource.password"));
+    }
 }
---
+++ src/main/java/com/staxrt/tutorial/model/Constants.java
@@ -0,0 +1,7 @@
+package com.staxrt.tutorial.model;
+
+public class Constants {
+
+    public static final String awsHostName="http://ec2-18-222-197-141.us-east-2.compute.amazonaws.com";
+    public static final int awsPortNumber=9191;
+}
